8db7b18647d6e5e1a842abb58d213d262e3b684d
saul <eternx@163.com>
2018-04-21 20:56:17 +0800

    <002> build(AnswerGeneratorTest)

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,30 @@
 package tw.core.generator;
 
+import javafx.beans.binding.When;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import tw.core.exception.OutOfRangeAnswerException;
+
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
 
+    private RandomIntGenerator randomIntGenerator;
+    private AnswerGenerator answerGenerator;
+
+    @Before
+    public void setUp() throws Exception {
+        randomIntGenerator = Mockito.mock(RandomIntGenerator.class);
+        answerGenerator = new AnswerGenerator(randomIntGenerator);
+    }
+
+    @Test
+    public void testGemerate() throws OutOfRangeAnswerException {
+        Mockito.when(randomIntGenerator.generateNums(9,4)).thenReturn("1 2 3 4");
+        Assert.assertEquals("1 2 3 4",answerGenerator.generate().toString());
+    }
 }
 
