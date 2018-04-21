a76eed633aaa9a50073749e790c29ad271a0826d
saul <eternx@163.com>
2018-04-21 21:46:32 +0800

    <004>  exchange(GameTest,GameControllerTest):the content of GameController is a mistake

--- src/test/java/tw/controllers/GameControllerTest.java
+++ src/test/java/tw/controllers/GameControllerTest.java
@@ -18,46 +18,4 @@ import java.util.stream.IntStream;
  */
 public class GameControllerTest {
 
-    private Game game;
-    private AnswerGenerator answerGenerator;
-
-    @Before
-    public void setUp() throws Exception {
-        answerGenerator = Mockito.mock(AnswerGenerator.class);
-        Mockito.when(answerGenerator.generate()).thenReturn(Answer.createAnswer("1 2 3 4"));
-        game=new Game(answerGenerator);
-    }
-
-    @Test
-    public void testGuess() throws OutOfRangeAnswerException {
-        GuessResult b = game.guess(Answer.createAnswer("1 2 3 4"));
-        Assert.assertEquals("4A0B",b.getResult());
-    }
-
-    @Test
-    public void testGuessFalse() throws OutOfRangeAnswerException {
-        GuessResult b = game.guess(Answer.createAnswer("9 2 3 4"));
-        Assert.assertEquals("3A0B", b.getResult());
-    }
-
-    @Test
-    public void testCheckStatus() {
-        Answer answer = Answer.createAnswer("9 2 3 4");
-        IntStream.range(1,5).boxed().map(x->answer).forEach(game::guess);
-        Assert.assertEquals(GameStatus.CONTINUE,game.checkStatus());
-        game.guess(Answer.createAnswer("1 2 3 4"));
-       Assert.assertEquals(GameStatus.SUCCESS,game.checkStatus());
-        game.guess(Answer.createAnswer("1 2 3 4"));
-        Assert.assertEquals(GameStatus.FAIL,game.checkStatus());
-    }
-
-    @Test
-    public void testCheckCoutinue() {
-        Answer answer = Answer.createAnswer("9 2 3 4");
-        IntStream.range(1,6).boxed().map(x->answer).forEach(game::guess);
-        Assert.assertTrue(game.checkCoutinue());
-        game.guess(Answer.createAnswer("1 2 3 4"));
-        Assert.assertFalse(game.checkCoutinue());
-    }
-
 }
